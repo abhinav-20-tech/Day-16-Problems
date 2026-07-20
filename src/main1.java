47e224681f9402af859fce0422a21bcef53f2166
Abhinav VK <abhinavkrishna4475@gamil.com>
2026-07-20 09:29:52 +0530

    Day 16

---
+++ src/main1.java
@@ -0,0 +1,10 @@
+public class main1 {
+    public static void main(String[] args) {
+        int n = 64;
+
+        while (n > 1) {
+            System.out.println(n);
+            n = n / 2;
+        }
+    }
+}
